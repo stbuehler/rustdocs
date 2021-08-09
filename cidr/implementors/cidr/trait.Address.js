da8635cfc49c5671db4e98702b2dcae066e73744
action-rs-deploy-doc <action-rs-deploy-doc@invalid>
2021-08-09 10:43:46 +0000

    doc upload for stbuehler/rust-cidr

--- cidr/implementors/cidr/trait.Address.js
+++ cidr/implementors/cidr/trait.Address.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["cidr"] = [{"text":"impl Into&lt;Option&lt;IpCidr&gt;&gt; for AnyIpCidr","synthetic":false,"types":[]}];
+implementors["cidr"] = [];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- cidr/implementors/core/default/trait.Default.js
+++ cidr/implementors/core/default/trait.Default.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {};
-implementors["bitstring"] = [{"text":"impl&lt;W:&nbsp;FixedBitString&gt; Default for BitLengthString&lt;W&gt;","synthetic":false,"types":[]}];
-implementors["serde"] = [{"text":"impl Default for IgnoredAny","synthetic":false,"types":[]}];
+implementors["bitstring"] = [{"text":"impl&lt;W:&nbsp;<a class=\"trait\" href=\"bitstring/fixed_bit_string/trait.FixedBitString.html\" title=\"trait bitstring::fixed_bit_string::FixedBitString\">FixedBitString</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"bitstring/struct.BitLengthString.html\" title=\"struct bitstring::BitLengthString\">BitLengthString</a>&lt;W&gt;","synthetic":false,"types":["bitstring::bit_length_string::BitLengthString"]}];
+implementors["serde"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"serde/de/struct.IgnoredAny.html\" title=\"struct serde::de::IgnoredAny\">IgnoredAny</a>","synthetic":false,"types":["serde::de::ignored_any::IgnoredAny"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
