ff6c426c495b52ea63b419adf136c04a6750be12
action-rs-deploy-doc <action-rs-deploy-doc@invalid>
2021-10-08 19:14:26 +0000

    doc upload for stbuehler/rust-bitstring

--- bitstring/implementors/core/default/trait.Default.js
+++ bitstring/implementors/core/default/trait.Default.js
@@ -1,10 +1,3 @@
 (function() {var implementors = {};
-implementors["bitstring"] = ["impl&lt;W:&nbsp;<a class=\"trait\" href=\"bitstring/fixed_bit_string/trait.FixedBitString.html\" title=\"trait bitstring::fixed_bit_string::FixedBitString\">FixedBitString</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"bitstring/struct.BitLengthString.html\" title=\"struct bitstring::BitLengthString\">BitLengthString</a>&lt;W&gt;",];
-
-            if (window.register_implementors) {
-                window.register_implementors(implementors);
-            } else {
-                window.pending_implementors = implementors;
-            }
-        
-})()
+implementors["bitstring"] = [{"text":"impl&lt;W:&nbsp;<a class=\"trait\" href=\"bitstring/fixed_bit_string/trait.FixedBitString.html\" title=\"trait bitstring::fixed_bit_string::FixedBitString\">FixedBitString</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"bitstring/struct.BitLengthString.html\" title=\"struct bitstring::BitLengthString\">BitLengthString</a>&lt;W&gt;","synthetic":false,"types":["bitstring::bit_length_string::BitLengthString"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ bitstring/source-files.js
@@ -0,0 +1,3 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["bitstring"] = {"name":"","dirs":[{"name":"bit_string","files":["mod.rs","traits.rs"]},{"name":"fixed_bit_string","files":["iter.rs","mod.rs","traits.rs"]},{"name":"utils","files":["bigendian.rs","mod.rs"]}],"files":["address.rs","bit_length_string.rs","lib.rs"]};
+createSourceSidebar();
