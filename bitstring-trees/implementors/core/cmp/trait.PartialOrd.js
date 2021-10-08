d079646af2f1da74b6fdc32077a2179ecd6c924f
action-rs-deploy-doc <action-rs-deploy-doc@invalid>
2021-10-08 19:15:01 +0000

    doc upload for stbuehler/rust-bitstring-trees

--- bitstring-trees/implementors/core/cmp/trait.PartialOrd.js
+++ bitstring-trees/implementors/core/cmp/trait.PartialOrd.js
@@ -1,10 +1,3 @@
 (function() {var implementors = {};
-implementors["bitstring"] = ["impl&lt;W:&nbsp;<a class=\"trait\" href=\"bitstring/fixed_bit_string/trait.FixedBitString.html\" title=\"trait bitstring::fixed_bit_string::FixedBitString\">FixedBitString</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a> for <a class=\"struct\" href=\"bitstring/struct.BitLengthString.html\" title=\"struct bitstring::BitLengthString\">BitLengthString</a>&lt;W&gt;",];
-
-            if (window.register_implementors) {
-                window.register_implementors(implementors);
-            } else {
-                window.pending_implementors = implementors;
-            }
-        
-})()
+implementors["bitstring"] = [{"text":"impl&lt;W:&nbsp;<a class=\"trait\" href=\"bitstring/fixed_bit_string/trait.FixedBitString.html\" title=\"trait bitstring::fixed_bit_string::FixedBitString\">FixedBitString</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a>&lt;<a class=\"struct\" href=\"bitstring/struct.BitLengthString.html\" title=\"struct bitstring::BitLengthString\">BitLengthString</a>&lt;W&gt;&gt; for <a class=\"struct\" href=\"bitstring/struct.BitLengthString.html\" title=\"struct bitstring::BitLengthString\">BitLengthString</a>&lt;W&gt;","synthetic":false,"types":["bitstring::bit_length_string::BitLengthString"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ bitstring-trees/source-files.js
@@ -0,0 +1,4 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["bitstring"] = {"name":"","dirs":[{"name":"bit_string","files":["mod.rs","traits.rs"]},{"name":"fixed_bit_string","files":["iter.rs","mod.rs","traits.rs"]},{"name":"utils","files":["bigendian.rs","mod.rs"]}],"files":["address.rs","bit_length_string.rs","lib.rs"]};
+sourcesIndex["bitstring_trees"] = {"name":"","dirs":[{"name":"map","files":["iter.rs","iter_full.rs","mod.rs"]},{"name":"set","files":["iter.rs","iter_full.rs","mod.rs"]}],"files":["lib.rs"]};
+createSourceSidebar();
