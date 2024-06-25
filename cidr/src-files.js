914aed679106555e59f953349c3e1bb8eeef2705
action-rs-deploy-doc <action-rs-deploy-doc@invalid>
2024-06-25 11:29:37 +0000

    doc upload for stbuehler/rust-cidr

--- cidr/src-files.js
+++ cidr/src-files.js
@@ -1,6 +1,6 @@
 var srcIndex = new Map(JSON.parse('[\
 ["bitstring",["",[["bit_string",[],["mod.rs","traits.rs"]],["fixed_bit_string",[],["iter.rs","mod.rs","traits.rs"]],["utils",[],["bigendian.rs","mod.rs"]]],["address.rs","bit_length_string.rs","lib.rs"]]],\
-["cidr",["",[["cidr",[["serde",[],["mod.rs"]]],["any.rs","combined.rs","direct.rs","from_str.rs","mod.rs"]],["inet",[["serde",[],["mod.rs"]]],["combined.rs","direct.rs","from_str.rs","mod.rs"]],["inet_pair",[],["combined.rs","direct.rs","mod.rs"]],["parsers",[],["combinators.rs","inetaddr.rs","mod.rs"]]],["address.rs","errors.rs","family.rs","inet_iterator.rs","internal_traits.rs","lib.rs","local_addr_parser.rs","num.rs","serde_common.rs","traits.rs"]]],\
+["cidr",["",[["cidr",[["serde",[],["mod.rs"]]],["any.rs","combined.rs","direct.rs","from_str.rs","mod.rs"]],["inet",[["serde",[],["mod.rs"]]],["combined.rs","direct.rs","from_str.rs","mod.rs"]],["inet_pair",[],["combined.rs","direct.rs","mod.rs"]],["parsers",[],["combinators.rs","inetaddr.rs","ipv4_short.rs","mod.rs"]]],["address.rs","errors.rs","family.rs","inet_iterator.rs","internal_traits.rs","lib.rs","local_addr_parser.rs","num.rs","serde_common.rs","traits.rs"]]],\
 ["serde",["",[["de",[],["format.rs","ignored_any.rs","impls.rs","mod.rs","seed.rs","size_hint.rs","value.rs"]],["private",[],["de.rs","doc.rs","mod.rs","ser.rs"]],["ser",[],["fmt.rs","impls.rs","impossible.rs","mod.rs"]]],["integer128.rs","lib.rs","macros.rs"]]]\
 ]'));
 createSrcSidebar();
