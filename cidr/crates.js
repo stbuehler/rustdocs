79ad57363e72a30c24411e8ef310ed6cda3c3292
action-rs-deploy-doc <action-rs-deploy-doc@invalid>
2024-09-30 14:45:18 +0000

    doc upload for stbuehler/rust-cidr

--- cidr/crates.js
+++ cidr/crates.js
@@ -1,1 +1,2 @@
 window.ALL_CRATES = ["bitstring","cidr","serde"];
+//{"start":21,"fragment_lengths":[11,7,8]}
--- cidr/trait.impl/core/iter/traits/double_ended/trait.DoubleEndedIterator.js
+++ cidr/trait.impl/core/iter/traits/double_ended/trait.DoubleEndedIterator.js
@@ -1,3 +1,9 @@
-(function() {var implementors = {
-"cidr":[["impl&lt;A: <a class=\"trait\" href=\"cidr/trait.Address.html\" title=\"trait cidr::Address\">Address</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/traits/double_ended/trait.DoubleEndedIterator.html\" title=\"trait core::iter::traits::double_ended::DoubleEndedIterator\">DoubleEndedIterator</a> for <a class=\"struct\" href=\"cidr/struct.InetAddressIterator.html\" title=\"struct cidr::InetAddressIterator\">InetAddressIterator</a>&lt;A&gt;"],["impl&lt;A: <a class=\"trait\" href=\"cidr/trait.Address.html\" title=\"trait cidr::Address\">Address</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/traits/double_ended/trait.DoubleEndedIterator.html\" title=\"trait core::iter::traits::double_ended::DoubleEndedIterator\">DoubleEndedIterator</a> for <a class=\"struct\" href=\"cidr/struct.InetIterator.html\" title=\"struct cidr::InetIterator\">InetIterator</a>&lt;A&gt;"]]
-};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
+(function() {
+    var implementors = Object.fromEntries([["cidr",[["impl&lt;A: <a class=\"trait\" href=\"cidr/trait.Address.html\" title=\"trait cidr::Address\">Address</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/traits/double_ended/trait.DoubleEndedIterator.html\" title=\"trait core::iter::traits::double_ended::DoubleEndedIterator\">DoubleEndedIterator</a> for <a class=\"struct\" href=\"cidr/struct.InetAddressIterator.html\" title=\"struct cidr::InetAddressIterator\">InetAddressIterator</a>&lt;A&gt;"],["impl&lt;A: <a class=\"trait\" href=\"cidr/trait.Address.html\" title=\"trait cidr::Address\">Address</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/traits/double_ended/trait.DoubleEndedIterator.html\" title=\"trait core::iter::traits::double_ended::DoubleEndedIterator\">DoubleEndedIterator</a> for <a class=\"struct\" href=\"cidr/struct.InetIterator.html\" title=\"struct cidr::InetIterator\">InetIterator</a>&lt;A&gt;"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[941]}
